cd1c9ecb5e0c01dcee6613375614b09cfc6b8680
zengtianshengz <zengtianshengz@163.com>
2018-01-21 00:38:34 +0800

    nt

---
+++ server/config.js
@@ -0,0 +1,25 @@
+/**
+ * @description: 文件或模块描述
+ * @author: zengtiansheng
+ * @update: 2017/10/21
+ */
+'use strict';
+
+module.exports = {
+    port: 9449,
+    jwtSecret: 'xcx-server-secret',
+    url: 'mongodb://localhost:27017/yqy',
+    appid: 'wx3bcac52d10a4c3f7',
+    secret: '372e7faf186e838d609346914e2b93c8', // 改
+    qnaccessKey:'mBoc0ssyOzWX35zHcYVvYw7W357OY-QvOeTqJ0D3',
+    qnsecretKey: 'X7vss6_hGyWrPWg9xro98sxRJxbTZxWxCeqONQeW',
+    session: {
+        name: 'SID',
+        secret: 'SID',
+        cookie: {
+            httpOnly: true,
+            secure:   false,
+            maxAge:   365 * 24 * 60 * 60 * 1000,
+        }
+    }
+}
