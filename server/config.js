f3890afb746a73e2c6f2674d7457bffb0a68e4ec
zengtianshengz <zengtianshengz@163.com>
2018-01-22 21:10:40 +0800

    nt

--- server/config.js
+++ server/config.js
@@ -5,21 +5,30 @@
  */
 'use strict';
 
-module.exports = {
+const env = process.env.NODE_ENV || 'development'
+let config = {}
+const common = {
     port: 9449,
+    url: 'mongodb://localhost:27017/xcx-dev',
     jwtSecret: 'xcx-server-secret',
-    url: 'mongodb://localhost:27017/yqy',
     appid: 'wx3bcac52d10a4c3f7',
-    secret: '372e7faf186e838d609346914e2b93c8', // 改
+    secret: '372e7faf186e838d609346914e2b93c8',
     qnaccessKey:'mBoc0ssyOzWX35zHcYVvYw7W357OY-QvOeTqJ0D3',
     qnsecretKey: 'X7vss6_hGyWrPWg9xro98sxRJxbTZxWxCeqONQeW',
-    session: {
-        name: 'SID',
-        secret: 'SID',
-        cookie: {
-            httpOnly: true,
-            secure:   false,
-            maxAge:   365 * 24 * 60 * 60 * 1000,
-        }
+}
+
+if (env === 'testing') {
+    config = {
+        port: 9448,
+        url: 'mongodb://localhost:27017/xcx-test',
     }
 }
+
+if (env === 'production') {
+    config = {
+        port: 9449,
+        url: 'mongodb://localhost:27017/xcx-prod',
+    }
+}
+
+module.exports = Object.assign(common, config)
